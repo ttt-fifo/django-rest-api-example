f757f9e1849d0eba1de65a2fc372496aaf446506
Todor Todorov <ttodorov@null.net>
2019-08-28 21:08:07 +0300

    Refactor: improve providers model
    Added field restrictions. String views imroved.

--- api_example/providers/models.py
+++ api_example/providers/models.py
@@ -3,28 +3,30 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Language(models.Model):
-    code = models.CharField(max_length=5, unique=True)
-    bidi = models.BooleanField()
-    name = models.CharField(max_length=50)
-    name_local = models.CharField(max_length=50)
+    code = models.CharField(max_length=5, unique=True, blank=False)
+    bidi = models.BooleanField(blank=False)
+    name = models.CharField(max_length=50, blank=False)
+    name_local = models.CharField(max_length=50, blank=False)
 
     def __str__(self):
-        return f'{self.code} ({self.name})'
+        return f'[{self.id}] {self.code} ({self.name})'
 
 
 class Currency(models.Model):
-    symbol = models.CharField(max_length=3)
+    symbol = models.CharField(max_length=3, blank=False)
 
     def __str__(self):
-        return self.symbol
+        return f'[{self.id}] {self.symbol}'
 
 
 class Provider(models.Model):
-    name = models.CharField(max_length=254, unique=True)
-    email = models.EmailField()
-    phonenumber = PhoneNumberField()
-    language = models.ForeignKey(Language, on_delete=models.PROTECT)
-    currency = models.ForeignKey(Currency, on_delete=models.PROTECT)
+    name = models.CharField(max_length=254, unique=True, blank=False)
+    email = models.EmailField(blank=False)
+    phonenumber = PhoneNumberField(blank=False)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT,
+                                 blank=False)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT,
+                                 blank=False)
 
     def __str__(self):
-        return self.name
+        return f'[{self.id}] {self.name}'
